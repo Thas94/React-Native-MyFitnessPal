d624daa1f9fe9ad0ad4e71c44ea9ae93092efc3b
NTC\0002228650 <Thabiso.Malematja@netcare.co.za>
2024-03-20 21:13:29 +0200

    Flatlist

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,14 +1,22 @@
 import { Link } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 import FoodListItem from '../components/FoodListItem';
 
+const foodItems = [
+  {label: 'Pizza', cal: 75, brand: 'Dominos'},
+  {label: 'Apple', cal: 100, brand: 'yesshs'},
+  {label: 'Banana', cal: 25, brand: 'lclc'}
+];
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <FoodListItem item={{label: 'Pizza', cal: 75, brand: 'Dominos'}}/>
-      <FoodListItem item={{label: 'Pizza', cal: 75, brand: 'Dominos'}}/>
+      <FlatList 
+        data={foodItems}
+        renderItem={({item}) => <FoodListItem item={item}/>}
+        contentContainerStyle={{gap: 5}}
+      />
 
     </View>
   );
@@ -18,8 +26,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    justifyContent: 'center',
     padding: 10,
-    gap: 5
   },
 });
