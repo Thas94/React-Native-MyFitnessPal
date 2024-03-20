00c3db73558eedc289257617d04c5f2bb946625c
NTC\0002228650 <Thabiso.Malematja@netcare.co.za>
2024-03-20 21:30:04 +0200

    Search bar

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,8 @@
 import { Link } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, Text, View, FlatList, TextInput, Button } from 'react-native';
 import FoodListItem from '../components/FoodListItem';
+import { useState } from 'react';
 
 const foodItems = [
   {label: 'Pizza', cal: 75, brand: 'Dominos'},
@@ -10,8 +11,18 @@ const foodItems = [
 ];
 
 export default function App() {
+
+  const [search, setSearch] = useState('');
+  const performSearch = () => {
+    console.warn('Searching: ', search);
+  }
+
   return (
     <View style={styles.container}>
+      <TextInput value={search} onChangeText={setSearch} placeholder='Search...' style={styles.input}/>
+      {search && <Button title='Search' onPress={performSearch}/>}
+
+      <Text>{search}</Text>
       <FlatList 
         data={foodItems}
         renderItem={({item}) => <FoodListItem item={item}/>}
@@ -28,4 +39,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     padding: 10,
   },
+  input: {
+    backgroundColor: '#f2f2f2',
+    padding: 20,
+    borderRadius: 20
+  }
 });
